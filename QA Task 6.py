a94f417c066a152bd69bed0c9c08318836cce3e9
Greg Rell <gregory.rell@universalorlando.com>
2019-11-21 16:51:57 -0500

    Vectorized and padded with the example code

--- QA Task 6.py
+++ QA Task 6.py
@@ -1,8 +1,6 @@
 #Imports
-import pickle
-import numpy as np
-from os import listdir
-import re
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 from FromKeras import parse_stories, get_stories #this file used from Keras examples https://keras.io/examples/babi_memnn/
 #Get training and test data
 
@@ -30,14 +28,16 @@ for story, question, answer in training_data:
     vocab = vocab.union(story)
     vocab = vocab.union(question)
     vocab.add(answer)
-print('the length of the vocab is',len(vocab))
+length_vocab=len(vocab)+1
+print('the length of the vocab is',length_vocab)
 
 #Determine the longest sentence length from both training and testing data
 
 longest_train_sentence=0
 longest_train_question=0
-
+story_text=[]
 for story,question,answer in training_data:
+    story_text.append(story)
 
     if len(story)>longest_train_sentence:
         longest_train_sentence=len(story)
@@ -58,10 +58,81 @@ print('the longest testing story is',longest_test_sentence,'longest test questio
 longest_story_sentence = max(longest_train_sentence,longest_test_sentence)
 longest_question_sentence=max(longest_train_question,longest_test_question)
 
+#Padding
+t=Tokenizer(filters=[]) #ensure that no filters are used. By default this will filter out punctuation.
+t.fit_on_texts(vocab)
+print(t.index_word)
+
+#Tokenize Story, question, answers
+
+story_1_indexes=[]
+story_1_indexes.append(story_text[0])
+story_1_indexes = t.texts_to_sequences(story_1_indexes)
+#story_1_indexes=pad_sequences(story_1_indexes,maxlen=longest_story_sentence)
+#print(story_text[0])
+#print(story_1_indexes[0])
+
+
+stories=[]
+questions=[]
+answers=[]
+
+for story, question, answer in training_data:
+    stories.append(story)
+    questions.append(question)
+    answers.append(answer)
+
+
+def toSequence(input, t):
+    sequenced = []
+    for x in input:
+        sequenced.append([val for sublist in t.texts_to_sequences(x) for val in sublist])
+    return sequenced
+
+
+story_sequences=toSequence(stories,t)
+question_sequences=toSequence(questions,t)
+answer_sequences=toSequence(answers,t)
+
+print(stories[0])
+print(questions[0])
+print(answers[0])
 
 
 
+#print(story_sequences[0])
+#print(question_sequences[0])
 
 
+# Create a function for vectorizing the stories, questions and answers: ****************************TAKEN******
+import numpy as np
 
+def vectorize_stories(data, word_index=t.word_index, max_story_len=longest_story_sentence,
+                      max_question_len=longest_question_sentence):
+    # vectorized stories:
+    X = []
+    # vectorized questions:
+    Xq = []
+    # vectorized answers:
+    Y = []
+
+    for story, question, answer in data:
+        # Getting indexes for each word in the story
+        x = [word_index[word.lower()] for word in story]
+        # Getting indexes for each word in the story
+        xq = [word_index[word.lower()] for word in question]
+        # For the answers
+        y = np.zeros(len(word_index) + 1)  # Index 0 Reserved when padding the sequences
+        y[word_index[answer]] = 1
+
+        X.append(x)
+        Xq.append(xq)
+        Y.append(y)
+
+    # Now we have to pad these sequences:
+    return (pad_sequences(X, maxlen=max_story_len), pad_sequences(Xq, maxlen=max_question_len), np.array(Y))
+
+inputs_train, questions_train, answers_train = vectorize_stories(training_data)
+
+#*******************************************************************************************/TAKEN
 
